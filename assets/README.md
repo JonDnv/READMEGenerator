f71a53eec53092d4f59eccb96c33c1f52a03c9bd
Jon Godwin <45859095+JonDnv@users.noreply.github.com>
2020-11-16 16:08:44 -0700

    Sample README and Started index.js

---
+++ assets/README.md
@@ -0,0 +1,41 @@
+# Project Title
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+
+## Description
+
+Sample Description
+
+## Installation
+
+Sample Installation Instructions
+
+## Usage
+
+Sample Usage
+
+## Contributing
+
+Sample Contributing
+
+## Tests
+
+Sample Tests
+
+## License
+
+Sample License
+
+## Questions
+
+Sample Questions
--- index.js
+++ index.js
@@ -1,3 +1,59 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What is your project's title?",
+    },
+    {
+      type: "input",
+      name: "instructions",
+      message: "Please enter instructions for your project.",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please enter usage information for your project.",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message:
+        "Please enter information on how someone can contribute to your project.",
+    },
+    {
+      type: "input",
+      name: "testInstructions",
+      message: "Please enter information on how someone can test your program.",
+    },
+    {
+      type: "checkbox",
+      name: "license",
+      choices: [
+        "Apache License 2.0",
+        "GNU GPLv3",
+        "ISC License",
+        "MIT License",
+      ],
+      message: "Please select the license type for your project.",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please enter your GitHub username.",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your email address",
+    },
+  ])
+  .then((data) => {
+    const filename = `./CreatedREADMEs/${projectTitle}_README.md`;
+    fs.writeFile(filename, ``, (err) =>
+      err ? console.log(err) : console.log("Success!!")
+    );
+  });
